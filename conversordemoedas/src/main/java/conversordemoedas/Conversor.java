3bb498bc1f37ee69ad2d3f19824326932e2037fc
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-03-26 17:24:01 -0300

    Sexto Commit

--- conversordemoedas/src/main/java/conversordemoedas/Conversor.java
+++ conversordemoedas/src/main/java/conversordemoedas/Conversor.java
@@ -1,19 +1,29 @@
 package conversordemoedas;
 import java.util.Scanner;
+// Programa que converte o valor inserido pelo usuário (após perguntar em qual moeda o valor será inserido) para outras moedas 
 public class Conversor {
     public static void main(String[] args) {
         // valores em março-2025!!
+
+        // inicializa as variaveis que serão utilizadas
         double real = 0.0;
         double dollar = 0.0;
         double euro = 0.0;
         double libra = 0.0;
         double qtd = 0.0;
         String moeda = "";
+
+
+        // inicializa o scanner 
         Scanner input = new Scanner(System.in);
+        // pergunta ao usuario qual será a moeda que ele usará
         System.out.println("Qual moeda você deseja converter?\nOpções: Real, Euro, Libra e Dollar.");
+
+        // armazena a escolha da moeda na variavel escolha com letra minuscula para evitar erros
         String escolha = input.nextLine().toLowerCase();
 
-        // teste para ver se a escolha está saindo todo em minusculo
+
+        // muda o valor das variaveis dependendo da escolha do usuario
         switch (escolha)
         {
             case "real":
@@ -48,14 +58,21 @@ public class Conversor {
                 moeda = "Libras";
                 break;
 
+            // caso o usuario escolha uma moeda inválida, exibe um erro: 
             default:
                 input.close();
-                throw new IllegalArgumentException("Insira uma moeda válida...");
+                throw new IllegalArgumentException("Moeda inválida...");
         }
 
+        // pede o valor que será convertido
         System.out.println("Insira a quantidade: ");
+        // armazena na variavel qtd
         qtd = input.nextDouble();
+
+        // fecha o inpu
         input.close();
+
+        // exibe o resultado e faz os calculos
         System.out.printf("%.2f %s %s%n", qtd, moeda, "convertido para: ");
         System.out.printf("%s %.2f%s%n", "- Reais:", (qtd * real), " reais");
         System.out.printf("%s %.2f%s%n", "- Dollars:", (qtd * dollar), " dollars");
