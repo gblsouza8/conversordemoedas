844eaaba6267ec0bcbeff173205e0a52142d0c9a
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-03-26 16:42:51 -0300

    Terceiro Commit

--- conversordemoedas/src/main/java/conversordemoedas/Conversor.java
+++ conversordemoedas/src/main/java/conversordemoedas/Conversor.java
@@ -1,7 +1,15 @@
 package conversordemoedas;
-
-public class Main {
+import java.util.Scanner;
+public class Conversor {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // valores em março-2025!!
+        int real, euro, libra, dollar; 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Qual moeda você deseja converter?\nOpções: Real, Euro, Libra e Dollar");
+        String escolha = input.nextLine().toLowerCase();
+
+        // teste para ver se a escolha está saindo todo em minusculo
+        System.out.println(escolha);
+
     }
 }
